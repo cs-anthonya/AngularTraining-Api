809d29629fc95eab500ba974962e0bc314a568fc
AnthonyA <anthonya@cloudstaff.com>
2022-11-16 08:23:02 +0800

    init

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::factory()->state([
+            'name' => 'Admin Admin',
+            'email' => 'admin@email.com',
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi',
+        ])->create();
+    }
+}
